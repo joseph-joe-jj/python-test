f11b1caddf93bec20afadc28e5747122a4c2d465
joseph-joe-jj <46422688+joseph-joe-jj@users.noreply.github.com>
2021-04-01 00:16:28 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,10 +11,8 @@ def funckeycheck(key, componentline):
         print("since the status is yes, execute {} stack {}".format(component,stack))
         print("invoke {}.sh".format(key))
         print('\n')
-        print('\n')
     else:
         print("since the status is no, ingnoring {} stack {}".format(component,stack))
-        print('\n')
         print('\n')
 def funcsub(componentline):
     if search("3dspace", componentline):
@@ -23,38 +21,29 @@ def funcsub(componentline):
         funckeycheck("internal",componentline)
     else:
         print("invalid input")
-
 def my_func(name):
     # subName = "hcplmappd"+"[1-2]+"+"l"
     subName = "{}{}{}".format("hcplmappd","[1-2]+","l")
     print("substring value : {}".format(subName))
     basePath = "/Apps/NPICOEDatamig/FP/Joe/test/server_details"
-    # sub = re.compile(r'')
     print(f'the parameter is : {name}')
     if search(subName, name):
-        # newlist = os.listdir( basePath + "/" + name)
         newlist = os.listdir("{}/{}".format(basePath,name))
         print(newlist)
         for line in newlist:
             print("selected line : " + line)
-            # content = open(basePath + "/" + name + "/" + line, "r")
             content = open("{}/{}/{}".format(basePath,name,line), "r")
             for componentline in content:
-                # print("component deatils : " + componentline)
-                print("component deatils : {}".format(componentline))
+                print("component details : {}".format(componentline))
                 if search("batch", componentline):
                     component = componentline.split(':')[0]
-                    # print("component : " + component)
                     print("component : {}".format(component))
                     print('\n')
-                    print('\n')
-                # stack = componentline.split('-')[1]
                 elif search("internal", componentline) or search("3dspace", componentline):
                     funcsub(componentline)
                 else:
-                    print(componentline + " is invalid component")
+                    print("{} is invalid component".format(componentline))
     else:
         print("wrong value")
         exit()
-
 my_func(sys.argv[1])
